b764718962c21ee32b9df72b29d0b05a10244923
Simon Loir <simon-loir@hotmail.com>
2021-11-18 13:55:41 +0100

    changed models order

--- src/eslintme-website/pages/index.tsx
+++ src/eslintme-website/pages/index.tsx
@@ -9,19 +9,19 @@ const Home: NextPage = () => {
                 <h1>Choose your model</h1>
                 <div className={style.chooseModel}>
                     <Model
-                        name='Airbnb'
-                        features={[
-                            'Semicolon required',
-                            'Trailing comma required',
-                            'No space before function call parenthesis',
-                        ]}
-                    />
-                    <Model
                         name='Google'
                         features={[
                             'Semicolon required',
                             'Trailing comma required',
                             'Template string preferred',
+                            'No space before function call parenthesis',
+                        ]}
+                    />
+                    <Model
+                        name='Airbnb'
+                        features={[
+                            'Semicolon required',
+                            'Trailing comma required',
                             'No space before function call parenthesis',
                         ]}
                     />
