919f8f1fd1e4bf2b66dffb0ad8931137227b7aa2
Simon Loir <simon-loir@hotmail.com>
2022-02-26 14:05:12 +0100

    working downloader for the rules

---
+++ src/eslintme-website/public/rules/readme.txt
@@ -0,0 +1,1 @@
+Rules should be added here
--- src/eslintme-website/rules_downloader.js
+++ src/eslintme-website/rules_downloader.js
@@ -2,4 +2,29 @@
  * This file
  */
 const all = require('eslint/conf/eslint-all').rules;
+const fs = require('fs');
+const fetch = require('node-fetch');
 const keys = Object.keys(all);
+let out = {};
+const baseURL =
+    'https://raw.githubusercontent.com/eslint/eslint/main/docs/rules/$name.md';
+
+async function get(url) {
+    return await (await fetch(url)).text();
+}
+
+async function download() {
+    console.log('Downloading rules data from github.');
+    out = {};
+    for (let i = 0; i < keys.length; i++) {
+        const rule = keys[i];
+        const url = baseURL.replace('$name', rule);
+        try {
+            fs.writeFileSync(`public/rules/${rule}.md`, await get(url));
+        } catch (error) {
+            console.error('Failed', error);
+        }
+    }
+}
+
+download();
